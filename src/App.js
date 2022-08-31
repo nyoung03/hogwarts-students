fd3a26ee7269c440b96f989f8dd0292b56d0bbc0
nyoung03 <vnfdlvdk37@gmail.com>
2022-08-31 16:59:21 +0900

    git page 에러로 인한 라우터 수정

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="*" element={<Main />} />
       </Routes>
